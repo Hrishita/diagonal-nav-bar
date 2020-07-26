2cb17b3278d3c006122baa1d13fffcf886e2b9c9
Hrishita <hrishu10.hd@gmail.com>
2020-07-26 17:43:29 +0530

    init commit

---
+++ app/src/main/java/com/example/adinugroho/diagonaltry/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.adinugroho.diagonaltry;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("");
+            getSupportActionBar().setHomeButtonEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+}
